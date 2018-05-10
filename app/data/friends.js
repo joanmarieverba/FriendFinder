86e0e3def526fec3ffd12e8b51d3f54c53b308d1
Joan Marie Verba <verba001@umn.edu>
2018-05-09 20:11:53 -0500

    routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,1 +1,23 @@
-"use strict"
+// DATA
+
+let friendArray = [
+    {
+        name: "me",
+        photo: "http://me.com",
+        scores: [
+                "1",
+                "1",
+                "1",
+                "1",
+                "1",
+                "1",
+                "1",
+                "1",
+                "1",
+                "1"
+         ]
+    }
+];
+
+// export the array
+module.exports = friendArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,34 +1,21 @@
-// ===============================================================================
+
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information
-// ===============================================================================
 
-let friendsData = require("..app/data/friends");
+let friendsData = require("../data/friends");
 
-// ===============================================================================
+
 // ROUTING
-// ===============================================================================
 
 module.exports = function (app) {
     // API GET Requests
 
-    app.get("/api/friends", function (req, res) {
-        res.json(friendsData);
+    app.get("/api/friends", function (req, result) {
+        result.json(friendsData);
     });
 
 
-
-    // API POST Requests
-    // Below code handles when a user submits a form and thus submits data to the server.
-    // In each of the below cases, when a user submits form data (a JSON object)
-    // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
-    // ---------------------------------------------------------------------------
-
-    app.post("/api/friends", function (req, res) {
-
+    app.post("/api/friends", function (req, result) {
+        console.log("result ", result);
 
     });
 
