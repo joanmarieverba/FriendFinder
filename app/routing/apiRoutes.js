c28fb5cfd3f5939b4b25a05e2c10fd0ce11dd06e
Joan Marie Verba <verba001@umn.edu>
2018-05-11 12:58:50 -0500

    changes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,11 +14,46 @@ module.exports = function (app) {
 
     //adds the input data to friendsData
     app.post("/api/friends", function (req, res) {
-        console.log("result ", req.body);
+       // console.log("result ", req.body);
         friendsData.push(req.body);
-        console.log("newfriendsData ", friendsData);
-        
+       // console.log("req.body ", req.body.scores);
+       // console.log("newfriendsData ", friendsData);
+        let differenceArray = [];
+        let difference;
+        let accumulatedDifference = 0;
+        //length-1 because the last entry is the input entry
+        for (let i=0; i<friendsData.length-1; i++){
+          //  console.log("scores ", friendsData[i].scores);
+            accumulatedDifference = 0;
+            // calculate the total difference for each stored entry
+            for (let index = 0; index<friendsData[i].scores.length; index++){
+             //   console.log("friendsData[i].scores[index] ", friendsData[i].scores[index]);
+             //   console.log(friendsData[i].scores[index], req.body.scores[index]);
+                difference = Math.abs(parseInt(friendsData[i].scores[index]) - parseInt(req.body.scores[index]));
+            //    console.log("difference ", difference);
+                accumulatedDifference = accumulatedDifference + difference;
+            };
+            differenceArray.push(accumulatedDifference);
+        };
+ //       console.log("accumulatedDifference ", accumulatedDifference);
+ //       console.log("differenceArray ", differenceArray);
+  //      res.json(bestMatch);
+        //find the index of the lowest value
+        let bestMatch = [];
+        let bestMatchIndex = 0;
+        for (let n = 0; n < differenceArray.length; n++){
+            if (differenceArray[bestMatchIndex] > differenceArray[n]){
+                bestMatchIndex = n;
+            }
+        };
+        let bestMatchName = friendsData[bestMatchIndex].name;
+        let bestMatchUrl = friendsData[bestMatchIndex].photo;
+        bestMatch.push(bestMatchName);
+        bestMatch.push(bestMatchUrl);
+        console.log(bestMatch);
     });
 
 
 };
+
+
