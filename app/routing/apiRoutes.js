038b1bc240f43f784f9580e74a9454ddd51994cd
Joan Marie Verba <verba001@umn.edu>
2018-05-11 13:22:55 -0500

    data retrieval works

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,32 +12,26 @@ module.exports = function (app) {
         res.json(friendsData);
     });
 
-    //adds the input data to friendsData
+
     app.post("/api/friends", function (req, res) {
-       // console.log("result ", req.body);
+        //adds the input data to friendsData
         friendsData.push(req.body);
-       // console.log("req.body ", req.body.scores);
-       // console.log("newfriendsData ", friendsData);
+
         let differenceArray = [];
         let difference;
         let accumulatedDifference = 0;
+
         //length-1 because the last entry is the input entry
         for (let i=0; i<friendsData.length-1; i++){
-          //  console.log("scores ", friendsData[i].scores);
             accumulatedDifference = 0;
             // calculate the total difference for each stored entry
             for (let index = 0; index<friendsData[i].scores.length; index++){
-             //   console.log("friendsData[i].scores[index] ", friendsData[i].scores[index]);
-             //   console.log(friendsData[i].scores[index], req.body.scores[index]);
                 difference = Math.abs(parseInt(friendsData[i].scores[index]) - parseInt(req.body.scores[index]));
-            //    console.log("difference ", difference);
                 accumulatedDifference = accumulatedDifference + difference;
             };
             differenceArray.push(accumulatedDifference);
         };
- //       console.log("accumulatedDifference ", accumulatedDifference);
- //       console.log("differenceArray ", differenceArray);
-  //      res.json(bestMatch);
+
         //find the index of the lowest value
         let bestMatch = [];
         let bestMatchIndex = 0;
@@ -51,6 +45,7 @@ module.exports = function (app) {
         bestMatch.push(bestMatchName);
         bestMatch.push(bestMatchUrl);
         console.log(bestMatch);
+        res.json(bestMatch);
     });
 
 
